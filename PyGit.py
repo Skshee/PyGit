80f065148ef7d295141f3beaaa4a424f18a1194a
Suvan <suvanshee@gmail.com>
2025-06-09 10:39:16 +0530

    Added Chapter 2 and Hash function

--- PyGit.py
+++ PyGit.py
@@ -15,9 +15,20 @@ class ObjectType(enum.Enum):
 
 def read_file(path):
     """Read contents of file at given path as bytes."""
-    with open(path, 'rb') as f:
+    with open(path, 'rb') as f: 
         return f.read()
 
+def hash_object(data, obj_type, write=True):
+    header = f'{obj_type} {len(data)}'.encode()  # Every git object has a header like 'blob 23', 'commit 45' etc
+    full_data = header + b'\x00' + data          # header + null byte + actual content (null byte acts as a delimiter between header and actual data)
+
+    sha1 = hashlib.sha1(full_data).hexdigest()  # Using the imported hashlib library's for sha1 encoding and storing it in hexadecimal
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])  # Converts raw SHA-1 to actual filepath
+        os.makedirs(os.path.dirname(path), exist_ok=True) # Ensures that the directory exists
+        with open(path, 'wb') as f:
+            f.write(zlib.compress(full_data))   # Stores objects in zlib compressed format
+    return sha1
 
 
 
